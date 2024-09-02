5298fbde193d8629000a4f924f1bd6678901f2a0
karan-devslane <karan.budhwani@devslane.com>
2024-09-02 17:16:01 +0530

    Added input component as per the UI

---
+++ src/shared-resources/types/Input.type.ts
@@ -0,0 +1,20 @@
+import { CSSProperties, ChangeEvent } from 'react';
+
+export interface InputProps {
+  onChange?: (event: ChangeEvent<HTMLInputElement>) => void;
+  placeholder?: string;
+  label?: string;
+  helperText?: string;
+  type?: 'email' | 'password' | 'text' | 'date' | 'file' | 'number' | 'time';
+  className?: string;
+  leadingIcon?: any;
+  trailingIcon?: any;
+  labelClassName?: string;
+  style?: CSSProperties;
+  disabled?: boolean;
+  error?: string;
+  showErrorText?: boolean;
+  centerAlignText?: boolean;
+  acceptSingleCharacter?: boolean;
+  value?: any;
+}
