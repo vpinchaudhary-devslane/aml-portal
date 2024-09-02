5b9f126a1e7fee5d899009211399f23448b4df60
karan-devslane <karan.budhwani@devslane.com>
2024-09-02 17:20:55 +0530

    Added Modal component as per the UI

---
+++ src/shared-resources/components/Icons/CloseIcon.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const CloseIcon: React.FC = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='36'
+    height='36'
+    viewBox='0 0 36 36'
+    fill='none'
+  >
+    <path
+      d='M23 13L13 23M13 13L23 23'
+      stroke='#1E1E1E'
+      strokeWidth='1.6'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+  </svg>
+);
+
+export default CloseIcon;
