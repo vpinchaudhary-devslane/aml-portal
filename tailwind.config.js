8fd7faeca9c01264074975efe757e714507d92cc
karan-devslane <karan.budhwani@devslane.com>
2024-09-02 17:18:36 +0530

    Added button component as per the UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,13 @@
 module.exports = {
   content: [ './index.html', './src/**/*.{js,ts,jsx,tsx}' ],
-  safelist: [ 'h-[3.75rem]', 'h-[6.25rem]', 'w-[3.75rem]', 'w-[6.25rem]' ]
+  safelist: [ 'h-[3.75rem]', 'h-[6.25rem]', 'w-[3.75rem]', 'w-[6.25rem]' ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#5988FF',
+        disabled: '#D9D9D9',
+        purple: '#b400ff',
+      }
+    }
+  }
 }
