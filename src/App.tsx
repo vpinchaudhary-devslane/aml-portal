52cf3ca4b9f93ac4ff58ec085f212dd58d3c184f
karan-devslane <karan.budhwani@devslane.com>
2024-09-06 16:54:52 +0530

    Added quicksand fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import store from 'store';
+import CollapsibleKeyboard from './shared-resources/components/CollapsibleKeyboard/CollapsibleKeyboard';
 
 const App: React.FC = () => (
   <Provider store={store}>
     <div className='h-full'>
       <h1>Welcome to AML Portal!</h1>
+      <div className='absolute top-1/2 right-0 -translate-y-1/2'>
+        <CollapsibleKeyboard />
+      </div>
     </div>
   </Provider>
 );
