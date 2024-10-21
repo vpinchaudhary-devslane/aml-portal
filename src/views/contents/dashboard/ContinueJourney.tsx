4f2ce7025a85c7ee22cca084c0a963f7491bdf74
Arsh <arsh@Arshs-MacBook-Air.local>
2024-10-21 14:16:06 +0530

    button disabled if there are no questions

--- src/views/contents/dashboard/ContinueJourney.tsx
+++ src/views/contents/dashboard/ContinueJourney.tsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import ContainerLayout from 'shared-resources/components/ContainerLayout/ContainerLayout';
 import { loggedInUserSelector } from 'store/selectors/auth.selector';
+import { questionsSetSelector } from 'store/selectors/questionSet.selector';
 
 const ContinueJourney: React.FC = () => {
   const navigate = useNavigate();
   const userSelector = useSelector(loggedInUserSelector);
-
+  const questionsSet = useSelector(questionsSetSelector);
+  const [questions, setQuestions] = useState<any>();
+  useEffect(() => {
+    if (questionsSet?.questions) {
+      setQuestions(questionsSet?.questions);
+    }
+  }, [questionsSet]);
   const handleResumeClick = () => {
     navigate('/questions');
   };
@@ -22,6 +29,7 @@ const ContinueJourney: React.FC = () => {
       }
       buttonText='RESUME'
       onButtonClick={handleResumeClick}
+      buttonDisabled={!questions?.length}
     />
   );
 };
